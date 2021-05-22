79621a42f7a72fb99f8a18362a699eaba1e4630e
Rondinelli Luquini <79580178+rondinellivieira@users.noreply.github.com>
2021-05-21 23:13:52 -0300

    Add files via upload

---
+++ smart-clock/app.js
@@ -0,0 +1,32 @@
+function showTime(){
+    var date = new Date();   
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "A.M.";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "P.M.";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("Display").innerText = time;
+    document.getElementById("Display").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+showTime();
+
+
+
+    
